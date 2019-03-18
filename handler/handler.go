42c6978157e28005ea49cc331797277b7aa2e6a4
ShiShusheng <2855845850@qq.com>
2019-03-19 04:13:12 +0800

    实现上传接口

--- handler/handler.go
+++ handler/handler.go
@@ -1,9 +1,11 @@
 package handler
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 //处理文件上传
@@ -17,6 +19,33 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		io.WriteString(w, string(data))
 	} else if r.Method == "POST" {
-		//接收文件流及存储到本地目录
+		// 接收文件流及存储到本地目录
+		file, head, err := r.FormFile("file")
+		if err != nil {
+			fmt.Printf("Failed to get data,err:%s\n", err.Error())
+		}
+		// 关闭文件句柄
+		defer file.Close()
+
+		// 创建本地文件接收文件流
+		newFile, err := os.Create("/Volumes/doc/tmp/" + head.Filename)
+		if err != nil {
+			fmt.Printf("Failed to create file,err:%s\n", err.Error())
+		}
+		defer newFile.Close()
+
+		// 将文件内容拷贝到新文件的buffer流中
+		_, err = io.Copy(newFile, file)
+		if err != nil {
+			fmt.Printf("Failed to save data into file,err:%s\n", err.Error())
+			return
+		}
+
+		http.Redirect(w, r, "/file/upload/suc", http.StatusFound)
 	}
+}
+
+// 上传成功提示
+func UploadSucHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Upload succeed!")
 }
--- main.go
+++ main.go
@@ -8,8 +8,9 @@ import (
 
 func main() {
 
-	//设定路由规则
+	// 设定路由规则
 	http.HandleFunc("/file/upload", handler.UploadHandler)
+	http.HandleFunc("/file/upload/suc", handler.UploadSucHandler)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Printf("Failed to start server,err:%s", err.Error())
